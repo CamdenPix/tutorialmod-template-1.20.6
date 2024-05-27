7456b7a829206f3c8aa31d9762b813bd81337b53
CamdenPix <camdengenta@gmail.com>
2024-05-27 12:06:12 -0500

    Added Ruby Item

---
+++ src/main/java/name/tutorialmod/item/ModItems.java
@@ -0,0 +1,35 @@
+package name.tutorialmod.item;
+
+import name.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+public class ModItems {
+
+    //Create item
+    //remember to add it to the en_us.json file for localization
+    //and a new json file in the models/item to point to a texture to use in the textures/item folder
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+    //Helper function to add an item to an ingredient tab
+    private static void addItemsToIngredientTab(FabricItemGroupEntries entries){
+        entries.add(RUBY);
+    }
+
+    //Helper function to register the item into Minecraft
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    //Driver program, Called when mod is initialized
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTab);
+    }
+}
